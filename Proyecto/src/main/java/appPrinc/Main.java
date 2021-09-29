f75c5ad626321fde260c695618de3f5d97f4210b
Alexis <alex10o@hotmail.com>
2021-09-29 02:49:08 -0300

    29-actuakizacion

--- Proyecto/src/main/java/appPrinc/Main.java
+++ Proyecto/src/main/java/appPrinc/Main.java
@@ -1,6 +1,6 @@
 package appPrinc;
 
-//import com.educacionittelecom.database.Conexion;
+import com.educacionittelecom.database.Conexion;
 import com.educacionittelecom.datosDAO.ClienteDAO;
 import com.educacionittelecom.datosVO.ClienteVO;
 
@@ -11,7 +11,10 @@ public class Main {
 		//Conexion.conectar();
 		//ClienteVO c = new ClienteVO(123,"Austry","Castillo","castilloaustry@gmail.com");
 		//ClienteDAO.insertar(c);
-		ClienteVO c = new ClienteVO(505,"Tito","Lindo","TITO@gmail.com");
+		//ClienteVO c = new ClienteVO(505,"Tito","Lindo","TITO@gmail.com");
+		//ClienteDAO.insertar(c);
+		//desde el formuluario no me toma todavia
+		ClienteVO c = new ClienteVO(2409,"Desde","Spring","Spring@live.com.ar");
 		ClienteDAO.insertar(c);
 	}
 
